e8a7f33c0358b3d5e5e0f0e02b471428d23a8e59
Ashutosh Kumar Singh <134187886+ashutoshpmv@users.noreply.github.com>
2024-10-20 09:05:45 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+// const used for constants and non-changing values.
+const a=123
+
+//let is used for values that can be changed with time.
+let myName="Ashu"
+
+/*
+1. var is not used now. earlier it was used.
+2. semicolon can/cant be used, no effect.
+*/
+
+console.log(a);
+
+// only Declaring a variable then it holds "undefined" value
+let accName;
+
+console.table([a,myName,accName])
