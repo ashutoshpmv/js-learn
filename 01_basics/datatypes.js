a8792fae41102cc62df22f60cd0ea6a7a7c03659
Ashutosh Kumar Singh <134187886+ashutoshpmv@users.noreply.github.com>
2024-10-20 09:26:28 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+// www.tc39.es --->>for ecma script standards of JS and mdn for normal documentation
+// number,string,bigint,boolean(t/f)
+
+//null-standalone value
+//undefined- abhi vaklue assign nahi hui hai
+//symbols-unique
+//object
+
+let myName="Ashutosh"
+console.log(typeof(myName));
+console.log(typeof null);
+console.log(typeof undefined);
+
+//Output => string object undefined
+
+//typeof(null)--->> object
+//typeof(undefined)--->> undefined
