ee0ce9ce8167b8c7d9ede32c1b2ded18114e6aa7
Ashutosh Kumar Singh <134187886+ashutoshpmv@users.noreply.github.com>
2024-11-14 16:10:01 +0000

    numbers and methods on numbers

---
+++ 02_Maths_Number/number.js
@@ -0,0 +1,30 @@
+const num1=100
+console.log(typeof num1)
+
+const num2=new Number(200)
+console.log(typeof num2);     //object
+console.log(num2);
+
+//convert to string and get its length
+console.log(num2.toString().length);
+
+//get decimal values(for e-commerce websites)
+console.log(num2.toFixed(3));  //200.000
+
+const newNum=123.8966
+console.log(newNum.toPrecision(3)); //124
+
+//set zeros COMMA a/c to US/INDIA SYSTEM.
+const hundreds=1000000000;
+console.log(hundreds.toLocaleString());  //to US-SYSTEM
+
+console.log(hundreds.toLocaleString('en-IN'));  //to US-SYSTEM
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(num2.MIN_VALUE);
+
+
+
+
+
