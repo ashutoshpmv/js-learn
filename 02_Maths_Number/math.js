c20f64e972b1c70ca9d1f9205375ef70789e243a
Ashutosh Kumar Singh <134187886+ashutoshpmv@users.noreply.github.com>
2024-11-14 17:09:51 +0000

    mATHS FUNCTION AND METHODS

--- 02_Maths_Number/math.js
+++ 02_Maths_Number/math.js
@@ -1,1 +1,25 @@
-console.log(Math.abs(-50));
+console.log(Math.abs(-50));   //removes negative sign
+console.log(Math.round(4.7));  //5
+console.log(Math.ceil(4.2));  //5
+console.log(Math.floor(4.9));  //4
+console.log(Math.min(1,2,3,4,5));  //1
+console.log(Math.max(1,2,3,4,5));  //5
+
+//TRIGNOMETRIC FXN
+console.log(Math.sin(Math.PI/6));  //0.5
+
+console.log(Math.sqrt(16));
+console.log(Math.pow(2,3));
+
+
+//generate a random number between n and m both inclusive 
+const n=1
+const m=6
+const random=Math.floor(Math.random() * (m - n + 1)) + n;
+console.log(random);
+
+
+
+
+
+
